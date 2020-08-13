3646f548545ee5d739e979ddf17c585d81f725f1
Ali Baghban <Ali.bgn@hotmail.com>
2020-08-13 16:26:28 +0430

    Just applied a simple tax rule

--- Tax_GUI/Main.java
+++ Tax_GUI/Main.java
@@ -4,26 +4,29 @@ public class Main {
     public static void main(String args[]) {
         int more = 0;
         int income = 0;
+        float tax = 0;
         boolean cont = true;
         while (cont) {
             try {
                 String str = JOptionPane.showInputDialog("Enter your income ");
                 income = Integer.parseInt(str);
-                if (income < 1230){
-                    JOptionPane.showMessageDialog(null,"You have not to pay tax");
-                }else {
-                    // your tax rule
+                if (income < 1230) {
+                    JOptionPane.showMessageDialog(null, "You have not to pay tax");
+                } else {
+                    tax = income * 30 / 100;
+                    JOptionPane.showMessageDialog(null, "Your Annual tax : " + tax + " $");
                 }
                 more = JOptionPane.showConfirmDialog(null, "Would you like to calculate another one's tax ?");
-                System.out.print(more);
-                if (more == 0 ){
+
+                if (more == 0) {
                     cont = true;
-                }else {
+                } else {
                     cont = false;
                 }
             } catch (Exception ex) {
                 JOptionPane.showMessageDialog(null, ex.getMessage());
             }
         }
+        System.exit(0);
     }
 }
